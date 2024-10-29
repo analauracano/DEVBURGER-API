6769cc750d57aa41d27f4cc6874c05df401c5361
analauracano <laura.fioratti@outlook.com>
2024-10-29 17:09:39 -0300

    atual

---
+++ src/routes.js
@@ -0,0 +1,32 @@
+import { Router } from 'express'
+import multer from 'multer'
+import multerConfig from './config/multer'
+import { password } from './config/database'
+import UserController from './app/controllers/UserController'
+import SessionController from './app/controllers/SessionController'
+import ProductController from './app/controllers/ProductController'
+import CategoryController from './app/controllers/CategoryController'
+import authMiddleware from './app/middlewares/auth'
+import OrderController from './app/controllers/OrderController'
+
+const routes = new Router()
+const upload = multer(multerConfig)
+
+routes.post('/users', UserController.store)
+routes.post('/session', SessionController.store)
+
+routes.use(authMiddleware)
+
+routes.post('/products', upload.single('file'), ProductController.store)
+routes.get('/products', ProductController.index)
+routes.put('/products/:id', upload.single('file'), ProductController.update)
+
+routes.post('/categories', CategoryController.store)
+routes.get('/categories', CategoryController.index)
+routes.put('/categories/:id', upload.single('file'), CategoryController.update)
+
+routes.post('/orders', upload.single('file'), OrderController.store)
+routes.get('/orders', OrderController.index)
+routes.put('/orders/:id', OrderController.update)
+
+export default routes
